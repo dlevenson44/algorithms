fab71daf763d48070a891e9d9bcea20c79ad8667
dlevenson44 <daniellevenson4@gmail.com>
2018-06-24 16:03:24 -0400

    added quicksort example

--- algorithms.js
+++ algorithms.js
@@ -169,3 +169,29 @@ console.log("Array after sorting: " + array);
 var array = [51, 24, 14, 33, 92, 48, 30, 11];
 mergeSort(array, 0, array.length-1);
 console.log("Array after sorting: " + array);
+
+
+
+// Quicksort Example
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var partition = function(array, p, r) {
+    var q = p;
+    for (var j = p; j < r; j ++) {
+       if (array[j] <= array[r]) {
+           swap(array, j, q);
+           q ++;
+       }
+    }
+    swap(array, r, q);
+    return q;
+};
+
+var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6];
+var q = partition(array, 0, array.length-1);
+console.log("Array after partitioning: " + array);
+
