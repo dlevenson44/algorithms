c66d60e45cf56fa4d8fbf0d3e1a22825ca4b600f
dlevenson44 <daniellevenson4@gmail.com>
2018-06-24 23:00:02 -0400

    added BFS code

--- algorithms.js
+++ algorithms.js
@@ -230,3 +230,65 @@ console.log("Array after partitioning: " + array);
 var m = quickSort(array, 0, array.length - 1);
 console.log("Array after sorting:   " + array);
 
+
+
+// BFS Implemented
+/* A Queue object for queue-like functionality over JavaScript arrays. */
+var Queue = function() {
+    this.items = [];
+};
+Queue.prototype.enqueue = function(obj) {
+    this.items.push(obj);
+};
+Queue.prototype.dequeue = function() {
+    return this.items.shift();
+};
+Queue.prototype.isEmpty = function() {
+    return this.items.length === 0;
+};
+
+
+var doBFS = function(graph, source) {
+    var bfsInfo = [];
+
+    for (var i = 0; i < graph.length; i++) {
+        bfsInfo[i] = {
+            distance: null,
+            predecessor: null };
+    }
+
+    bfsInfo[source].distance = 0;
+
+    var queue = new Queue();
+    queue.enqueue(source);
+    
+    while (!queue.isEmpty()) {
+        var vertex = queue.dequeue();
+        for (var j = 0; j < graph[vertex].length; j ++) {
+            var neighbor = graph[vertex][j];
+            if (bfsInfo[neighbor].distance === null){
+                bfsInfo[neighbor].distance = bfsInfo[vertex].distance + 1;
+                bfsInfo[neighbor].predecessor = vertex;
+                queue.enqueue(neighbor);
+            }
+        }
+    }
+
+    return bfsInfo;
+};
+
+
+var adjList = [
+    [1],
+    [0, 4, 5],
+    [3, 4, 5],
+    [2, 6],
+    [1, 2],
+    [1, 2, 6],
+    [3, 5],
+    []
+    ];
+var bfsInfo = doBFS(adjList, 3);
+for (var i = 0; i < adjList.length; i++) {
+    console.log("vertex " + i + ": distance = " + bfsInfo[i].distance + ", predecessor = " + bfsInfo[i].predecessor);
+}
